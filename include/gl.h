9b8109b8791f19dc66868f87f4de71668bf7b80a
dragonly <liyilongko@gmail.com>
2017-12-21 13:27:59 +0800

    init with glfw3

---
+++ include/gl.h
@@ -0,0 +1,11 @@
+//
+// Created by Dragonly on 21/12/2017.
+//
+
+#ifndef UNREAL_GL_H
+#define UNREAL_GL_H
+
+#define GLFW_INCLUDE_GLCOREARB
+#include <GLFW/glfw3.h>
+
+#endif //UNREAL_GL_H
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "gl.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
